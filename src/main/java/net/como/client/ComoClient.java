e39e1ca59c0c6c2e6b88ee03b608b8357dedf290
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2023-12-19 22:35:19 +0000

    Fix client
    Update to 1.20.4

--- src/main/java/net/como/client/ComoClient.java
+++ src/main/java/net/como/client/ComoClient.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example;
+package net.como.client;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
--- src/main/java/net/como/client/mixin/ExampleMixin.java
+++ src/main/java/net/como/client/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.como.client.mixin;
 
-import net.fabricmc.example.ComoClient;
+import net.como.client.ComoClient;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
