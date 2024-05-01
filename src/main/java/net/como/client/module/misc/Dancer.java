9caf0c49c22ebdf96a3b67e0efb324f7f3f41aca
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2024-05-01 17:17:52 +0100

    Add speedy hands

--- src/main/java/net/como/client/module/misc/Dancer.java
+++ src/main/java/net/como/client/module/misc/Dancer.java
@@ -4,6 +4,8 @@ import net.como.client.ComoClient;
 import net.como.client.event.EventHandler;
 import net.como.client.event.impl.PlayerTickEvent;
 import net.como.client.module.Module;
+import net.minecraft.client.option.SimpleOption;
+import net.minecraft.util.Arm;
 import net.minecraft.util.Hand;
 
 public class Dancer extends Module {
@@ -11,22 +13,32 @@ public class Dancer extends Module {
         this.setDescription("Swing your arms from side to side!");
     }
 
-    private int i = 0;
-    private boolean mainHand = true;
+    private Arm originalArm;
+
+    private SimpleOption<Arm> getArmOption() {
+        return ComoClient.getInstance().getClient().options.getMainArm();
+    }
+
+    @Override
+    protected void onEnable() {
+        originalArm = getArmOption().getValue();
+        super.onEnable();
+    }
+
+    @Override
+    protected void onDisable() {
+        super.onDisable();
+        getArmOption().setValue(originalArm);
+    }
 
     @EventHandler
     public void onTick(PlayerTickEvent event) {
         // Check that your hands are empty (this is annoying on servers)
         if (!ComoClient.getInstance().me().getMainHandStack().isEmpty() || !ComoClient.getInstance().me().getOffHandStack().isEmpty()) return;
 
-        i++;
-
-        if (i % 4 != 0) return; // Every 4 ticks
-
-        Hand hand = mainHand ? Hand.MAIN_HAND : Hand.OFF_HAND; // Get the hand
-        mainHand = !mainHand; // Switch hands
-
-        // Swing the arm
-        ComoClient.getInstance().me().swingHand(hand);
+        // There you go you smelly little sausage
+        SimpleOption<Arm> option = getArmOption();
+        option.setValue(option.getValue().getOpposite());
+        ComoClient.getInstance().me().swingHand(Hand.MAIN_HAND);
     }
 }
