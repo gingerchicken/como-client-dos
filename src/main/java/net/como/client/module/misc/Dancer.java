589b0e1ad4a679c2008c98376cba9a390a2bde0a
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2024-05-09 23:30:10 +0100

    Update Dancer

--- src/main/java/net/como/client/module/misc/Dancer.java
+++ src/main/java/net/como/client/module/misc/Dancer.java
@@ -13,6 +13,9 @@ public class Dancer extends Module {
         this.setDescription("Swing your arms from side to side!");
     }
 
+    final private double packetsPerSecond = 20; // TODO make this a setting
+    private DancerThread dancerThread;
+
     private Arm originalArm;
 
     private SimpleOption<Arm> getArmOption() {
@@ -23,22 +26,52 @@ public class Dancer extends Module {
     protected void onEnable() {
         originalArm = getArmOption().getValue();
         super.onEnable();
+
+        // Start the thread
+        dancerThread = new DancerThread();
+        dancerThread.start();
     }
 
     @Override
     protected void onDisable() {
         super.onDisable();
+
+        dancerThread.interrupt();
+
         getArmOption().setValue(originalArm);
     }
 
-    @EventHandler
-    public void onTick(PlayerTickEvent event) {
+    private void sendPacket() {
         // Check that your hands are empty (this is annoying on servers)
         if (!ComoClient.getInstance().me().getMainHandStack().isEmpty() || !ComoClient.getInstance().me().getOffHandStack().isEmpty()) return;
 
-        // There you go you smelly little sausage
+        // Get the arm
         SimpleOption<Arm> option = getArmOption();
+
+        // Change the arm to the opposite
         option.setValue(option.getValue().getOpposite());
+
+        // Swing the hand
         ComoClient.getInstance().me().swingHand(Hand.MAIN_HAND);
+    }
+
+    protected class DancerThread extends Thread {
+        @Override
+        public void run() {
+            while (isEnabled()) {
+                // Sleep for the amount of time it takes to send a packet
+                try {
+                    Thread.sleep((long) (1000 / packetsPerSecond));
+                } catch (InterruptedException e) {
+                    break;
+                }
+
+                // Ensure that the module is still enabled (after the sleep)
+                if (!isEnabled()) break;
+
+                // Send the packet
+                sendPacket();
+            }
+        }
     }
 }
